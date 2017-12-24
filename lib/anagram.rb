b9ec80808e5e0a4325a1525d59bbd9ca7bbbf8d4
Eddie Melo <heriberto@melo.nyc>
2017-12-24 20:28:37 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,6 +9,6 @@ class Anagram
   def match(words)
     puts "#{word},#{words}"
     result = []
-    words.detect {|reversed_word| reversed_word.sort == word.sort}
+    words.detect {|reversed_word| reversed_word.chars.sort == word.sort}
   end
 end
